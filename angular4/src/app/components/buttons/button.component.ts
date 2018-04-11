9fd1c81ea606789574879bda0d939be9e2a08359
Gustavo Kons de Souza <gustavokons1@gmail.com>
2018-04-10 22:52:58 -0300

    Iterations in angular4 boilerplate

--- angular4/src/app/components/buttons/button.component.ts
+++ angular4/src/app/components/buttons/button.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'cta',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
+
 export class ButtonComponent {
-  title = 'My App';
+  @Input() col: string;
+
+  constructor() {
+    this.theme = 'Another value';
+  }
 }
