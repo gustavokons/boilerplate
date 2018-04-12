1c61680ab06c29d3335fa55510de6ee0c34256fb
Gustavo Kons <gustavokons1@gmail.com>
2018-04-12 15:59:25 -0300

    Update angular4 boilerplate

--- angular4/src/app/app.module.ts
+++ angular4/src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { RdUIKit } from './modules/rd-uikit';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
+import { AboutComponent } from './about.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     RdUIKit
   ],
   providers: [],
